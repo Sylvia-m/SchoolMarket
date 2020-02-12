267d0836497a5c6d6285e53e0463f7f4420c3a52
Sylvia-m <m13279536765@163.com>
2020-02-12 22:47:13 +0800

    :beers: update ImageUtil

--- src/main/java/com/zjm/schoolmarket/util/ImageUtil.java
+++ src/main/java/com/zjm/schoolmarket/util/ImageUtil.java
@@ -9,11 +9,11 @@ import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Positions;
 
 public class ImageUtil {
-	//获取classpath的绝对值路径	
-	public static void main(String[] args) throws IOException {
-		String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-		Thumbnails.of(new File("C:\\Users\\12614\\Desktop\\graduatioprogram\\images\\xiaohuangren.jpg")).size(200, 200)
-		.watermark(Positions.BOTTOM_RIGHT,ImageIO.read(new File(basePath+"/zjm.png")),0.5f)
-		.outputQuality(0.8f).toFile("C:\\Users\\12614\\Desktop\\graduatioprogram\\images\\xiaohuangrennew.jpg");
-	}	
+    //获取classpath的绝对值路径
+    public static void main(String[] args) throws IOException {
+        String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        Thumbnails.of(new File(basePath + "\\xiaohuangren.jpg")).size(200, 200)
+                .watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "\\zjm.png")), 0.5f)
+                .outputQuality(0.8f).toFile(basePath + "\\xiaohuangrennew.jpg");
+    }
 }
