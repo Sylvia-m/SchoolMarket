5e2503d4a8d64f553f7f58b8367e567cd98b0dd8
Sylvia-m <m13279536765@163.com>
2020-02-13 23:00:00 +0800

    :beers: add PathUtil and update ImageUtil

---
+++ src/main/java/com/zjm/schoolmarket/util/PathUtil.java
@@ -0,0 +1,25 @@
+package com.zjm.schoolmarket.util;
+
+/*
+提供两类路径：1.依据执行环境的不同提供根路径（项目图片所需要存放的路径）
+ */
+public class PathUtil {
+    private static String separator = System.getProperty("file.separator"); //获取系统的执行属性
+    public static String getImgBasePath(){
+        String os = System.getProperty("os.name"); //根据不同的操作系统选择不同的根目录
+        String basePath = "";
+        if(os.toLowerCase().startsWith("win")){
+            basePath="D:\\allkinds\\images";
+        }else{
+            basePath="/home/zjm/images";
+        }
+        basePath =  basePath.replace("/", separator);
+        return basePath;
+    }
+
+    //相对子路径  获取店铺图片的存储路径
+    public static String getShopImagePath(long shopId){
+        String imagePath = "D:\\allkinds\\upload\\item\\shop"+shopId+"/";
+        return imagePath.replace("/",separator); //返回项目图片的子路径
+    }
+}
