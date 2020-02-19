a70cbb074e42bc87ab2cd01bd6ad745bd1e90b78
Sylvia-m <m13279536765@163.com>
2020-02-19 21:10:12 +0800

    :beers: update shopoperation.js

--- src/main/java/com/zjm/schoolmarket/service/impl/AreaServiceImpl.java
+++ src/main/java/com/zjm/schoolmarket/service/impl/AreaServiceImpl.java
@@ -10,12 +10,17 @@ import com.zjm.schoolmarket.entity.Area;
 import com.zjm.schoolmarket.service.AreaService;
 
 @Service
-public class AreaServiceImpl implements AreaService{
-	@Autowired
-	private AreaDao areaDao;
-	@Override
-	public List<Area> getAreaList() {		
-		return areaDao.queryArea();
-	}
-	
+public class AreaServiceImpl implements AreaService {
+
+    private final AreaDao areaDao;
+
+    public AreaServiceImpl(AreaDao areaDao) {
+        this.areaDao = areaDao;
+    }
+
+    @Override
+    public List<Area> getAreaList() {
+        return areaDao.queryArea();
+    }
+
 }
