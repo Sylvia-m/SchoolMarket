96dac72d6776e6e36882f89dbb90644a6c7891b0
Sylvia-m <m13279536765@163.com>
2020-02-19 21:09:16 +0800

    :beers: add ShopCategoryService.java and ShopCategoryServiceImpl.java

---
+++ src/main/java/com/zjm/schoolmarket/service/impl/ShopCategoryServiceImpl.java
@@ -0,0 +1,19 @@
+package com.zjm.schoolmarket.service.impl;
+
+import com.zjm.schoolmarket.dao.ShopCategoryDao;
+import com.zjm.schoolmarket.entity.ShopCategory;
+import com.zjm.schoolmarket.service.ShopCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ShopCategoryServiceImpl implements ShopCategoryService {
+    @Autowired
+    private ShopCategoryDao shopCategoryDao;
+    @Override
+    public List<ShopCategory> getShopCategoryList(ShopCategory shopCategoryCondition) {
+        return shopCategoryDao.queryShopCategory(shopCategoryCondition);
+    }
+}
