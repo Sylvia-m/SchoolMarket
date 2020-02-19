5934fafd937afcb03301cdb6b5ce89595f68eff5
Sylvia-m <m13279536765@163.com>
2020-02-19 21:04:40 +0800

    :beers: add ShopCategoryDao.xml and ShopCategoryDao.java

---
+++ src/main/java/com/zjm/schoolmarket/dao/ShopCategoryDao.java
@@ -0,0 +1,17 @@
+package com.zjm.schoolmarket.dao;
+
+import com.zjm.schoolmarket.entity.ShopCategory;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ShopCategoryDao {
+    /**
+     * 根据传入的查询条件返回店铺类别列表
+     *
+     * @param shopCategoryCondition
+     * @return
+     */
+
+    List<ShopCategory> queryShopCategory(@Param("shopCategoryCondition") ShopCategory shopCategoryCondition);
+}
