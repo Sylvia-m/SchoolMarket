d7f4b55c5da3229abb7fa4f8f9888160a6100ff0
Sylvia-m <m13279536765@163.com>
2020-02-28 20:10:53 +0800

    :beers: 前端修改店铺功能

--- src/main/java/com/zjm/schoolmarket/dao/ShopDao.java
+++ src/main/java/com/zjm/schoolmarket/dao/ShopDao.java
@@ -1,9 +1,21 @@
 package com.zjm.schoolmarket.dao;
 
 import com.zjm.schoolmarket.entity.Shop;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 
 public interface ShopDao {
+	/***
+	 * 分页查询店铺，可输入的条件有：店铺名（模糊查询），店铺状态，店铺类别，区域Id，owner
+	 * @param shopCondition
+	 * @param rowIndex 从第几行开始取数据
+	 * @param pageSize 返回的条数
+	 * @return
+	 */
+	List<Shop> queryShopList(@Param("shopCondition")Shop shopCondition,@Param("rowIndex")int rowIndex,@Param("pageSize")int pageSize);
+
 	/**
 	 * 通过shop id查询店铺
 	 *
--- src/main/webapp/resources/js/shop/shopoperation.js
+++ src/main/webapp/resources/js/shop/shopoperation.js
@@ -5,55 +5,58 @@
  */
 $(function () {
     // 从URL里获取shopId参数的值
-    //var shopId = getQueryString('shopId');
+    var shopId = getQueryString('shopId');
     // 由于店铺注册和编辑使用的是同一个页面，
     // 该标识符用来标明本次是添加还是编辑操作
-    //var isEdit = shopId ? true : false;
+    var isEdit = shopId ? true : false;
     // 用于店铺注册时候的店铺类别以及区域列表的初始化的URL
     var initUrl = '/SchoolMarket/shopadmin/getshopinitinfo';
     // 注册店铺的URL
     var registerShopUrl = '/SchoolMarket/shopadmin/registershop';
-    alert(initUrl);
     // 编辑店铺前需要获取店铺信息，这里为获取当前店铺信息的URL
-    //var shopInfoUrl = "/SchoolMarket/shopadmin/getshopbyid?shopId=" + shopId;
+    var shopInfoUrl = '/SchoolMarket/shopadmin/getshopbyid?shopId=' + shopId;
     // 编辑店铺信息的URL
-    //var editShopUrl = '/SchoolMarket/shopadmin/modifyshop';
+    var editShopUrl = '/SchoolMarket/shopadmin/modifyshop';
     // 判断是编辑操作还是注册操作
-    //if (!isEdit) {
-    getShopInitInfo();
-    //} else {
-    //   getShopInfo( );
-    //}
-    // 通过店铺Id获取店铺信息
-    //function getShopInfo() {
-    //$.getJSON(initUrl, function(data) {
-    //if (data.success) {
-    // 若访问成功，则依据后台传递过来的店铺信息为表单元素赋值
-    //var shop = data.shop;
-    //$('#shop-name').val(shop.shopName);
-    //$('#shop-addr').val(shop.shopAddr);
-    //$('#shop-phone').val(shop.phone);
-    //$('#shop-desc').val(shop.shopDesc);
-    // 给店铺类别选定原先的店铺类别值
-    //var shopCategory = '<option data-id="'
-    //    + shop.shopCategory.shopCategoryId + '" selected>'
-    //    + shop.shopCategory.shopCategoryName + '</option>';
-    //var tempAreaHtml = '';
-    // 初始化区域列表
-    //data.areaList.map(function(item, index) {
-    //    tempAreaHtml += '<option data-id="' + item.areaId + '">'
-    //        + item.areaName + '</option>';
-    //});
-    //$('#shop-category').html(shopCategory);
-    // 不允许选择店铺类别
-    //$('#shop-category').attr('disabled', 'disabled');
-    //$('#area').html(tempAreaHtml);
-    // 给店铺选定原先的所属的区域
-    //$("#area option[data-id='" + shop.area.areaId + "']").attr(
-    //    "selected", "selected");
-    //}
-    // });
-    //}
+    if (!isEdit) {
+        getShopInitInfo();
+    } else {
+        getShopInfo(shopId);
+    }
+
+    // 通过店铺Id获取店铺信息和区域列表信息
+    function getShopInfo(shopId) { //方法：获取表单数据并提交到后台
+        //try {
+        $.getJSON(shopInfoUrl, function (data) {
+            if (data.success) {
+                // 若访问成功，则依据后台传递过来的店铺信息为表单元素赋值
+                var shop = data.shop;
+                $('#shop-name').val(shop.shopName);
+                $('#shop-addr').val(shop.shopAddr);
+                $('#shop-phone').val(shop.phone);
+                $('#shop-desc').val(shop.shopDesc);
+                // 给店铺类别选定原先的店铺类别值
+                var shopCategory = '<option data-id="'
+                    + shop.shopCategory.shopCategoryId + '" selected>'
+                    + shop.shopCategory.shopCategoryName + '</option>';
+                var tempAreaHtml = '';
+                // 初始化区域列表
+                data.areaList.map(function (item, index) {
+                    tempAreaHtml += '<option data-id="' + item.areaId + '">'
+                        + item.areaName + '</option>';
+                });
+                $('#shop-category').html(shopCategory);
+                //不允许选择店铺类别
+                $('#shop-category').attr('disabled', 'disabled');
+                $('#area').html(tempAreaHtml);
+                // 给店铺选定原先的所属的区域
+                $("#area option[data-id='" + shop.area.areaId + "']").attr("selected", "selected");
+            }
+        });//}catch (error) {
+           // console.log(error.message);
+        //}
+    }
+
     // 取得所有二级店铺类别以及区域信息，并分别赋值进类别列表以及区域列表
     function getShopInitInfo() {
         $.getJSON(initUrl, function (data) {
@@ -72,16 +75,16 @@ $(function () {
                 $('#area').html(tempAreaHtml);
             }
         });
-
+    }
 
         // 提交按钮的事件响应，分别对店铺注册和编辑操作做不同响应
         $('#submit').click(function () {
             // 创建shop对象
             var shop = {};
-            //if (isEdit) {
+            if (isEdit) {
             // 若属于编辑，则给shopId赋值
-            //shop.shopId = shopId;
-            //}
+            shop.shopId = shopId;
+            }
             // 获取表单里的数据并填充进对应的店铺属性中
             shop.shopName = $('#shop-name').val();
             shop.shopAddr = $('#shop-addr').val();
@@ -95,8 +98,8 @@ $(function () {
             };
             // 选择选定好的区域信息
             shop.area = {
-                areaId : $('#area').find('option').not(function() {
-                   return !this.selected;
+                areaId: $('#area').find('option').not(function () {
+                    return !this.selected;
                 }).data('id')
             };
             // 获取上传的图片文件流
@@ -116,7 +119,7 @@ $(function () {
             formData.append('verifyCodeActual', verifyCodeActual);
             // 将数据提交至后台处理相关操作
             $.ajax({
-                url: (registerShopUrl),
+                url: (isEdit ? editShopUrl : registerShopUrl),
                 type: 'POST',
                 data: formData,
                 contentType: false,
@@ -125,10 +128,10 @@ $(function () {
                 success: function (data) {
                     if (data.success) {
                         $.toast('提交成功！');
-                        //if (!isEdit) {
+                        if (!isEdit) {
                         // 若为注册操作，成功后返回店铺列表页
-                        //   window.location.href = "/SchoolMarket/shopadmin/shoplist";
-                        //}
+                            window.location.href = "/SchoolMarket/shopadmin/shoplist";
+                        }
                     } else {
                         $.toast('提交失败！' + data.errMsg);
                     }
@@ -137,7 +140,7 @@ $(function () {
                 }
             });
         });
-    }
+
 })
 
 
